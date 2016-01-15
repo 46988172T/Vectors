8955ef200f918555b54e3645beee82cdbc5d9578
Leo! <leo@leoleo.com>
2016-01-15 19:36:36 +0100

    Initial Commit

---
+++ src/GrupOrdenatFiguraGeometricaViaTaula.java
@@ -0,0 +1,5 @@
+/**
+ * Created by 46988172t on 15/01/16.
+ */
+public class GrupOrdenatFiguraGeometricaViaTaula {
+}
---
+++ src/ProvaGrupOrdenatFiguraGeometricaViaTaula.java
@@ -0,0 +1,25 @@
+/*
+Dissenyeu la classe GrupOrdenatFiguraGeometricaViaTaula per gestionar un grup de figures geomètriques emmagatzemades en una taula del Java,
+amb els requeriments següents:
+- El codi de la figura és identificador dins la taula.
+- Les figures geomètriques estan permanentment ordenades pel codi.
+- La classe ha de proporcionar els mètodes bàsics per a la gestió de taules de figures geomètriques (afegir, cercar per codi, extreure, veure quina figura
+    hi ha en una posició determinada, buidar el grup…).
+- Les cerques per codi dins la taula s’han d’efectuar de la manera més eficient possible (cerca dicotòmica).
+- La classe ha de sobreescriure els mètode equals() heretats de la classe Object.
+
+Cal aprofitar, en el que sigui factible, els mètodes proporcionats per la classe Arrays.
+Si cal, podeu millorar el disseny de les classes de la jerarquia que neix a FiguraGeometrica afegint els mètodes i/o característiques que corresponguin.
+- Desenvolupeu una altra classe, anomenada ProvaGrupOrdenatFiguraGeometricaViaTaula, pertanyent a un paquet anomenat proves, que contingui un mètode main() en
+què es comprovi el funcionament dels mètodes dissenyats en la classe GrupOrdenatFiguraGeometricaViaTaula.
+Allà on ho considereu oportú, utilitzeu excepcions (pròpies o de les que proporciona el Java).
+ */
+
+/**
+ * Created by 46988172t on 15/01/16.
+ */
+public class ProvaGrupOrdenatFiguraGeometricaViaTaula {
+    public static void main (String[] args){
+
+    }
+}
